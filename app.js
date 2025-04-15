a74ef0c84205793ea955e159f772aba03ac83208
Matthew McTighe <matthewmctighe1@gmail.com>
2025-04-15 12:08:56 -0700

    cleaning up files

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./api/users')
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
-
-app.listen(port, () => {
-    console.log(`Example app is listening on port ${port}`)
-});
+let tasks = [];
 
 
-//This example shows a middleware sub-stack that handles GET requests to the /user/:id path.
-/*
-app.get('/user/:id', (req, res, next) => {
-    // if the user ID is 0, skip to the next route
-    if (req.params.id === '0') next('route')
-    // otherwise pass the control to the next middleware function in this stack
-    else next()
-  }, (req, res, next) => {
-    // send a regular response
-    res.send('regular')
-  })
-  
-  // handler for the /user/:id path, which sends a special response
-  app.get('/user/:id', (req, res, next) => {
-    res.send('special')
-  })
+const requestLogger = function(req,res, next){
+    const startTime = new Date();
 
+    res.on("finish", ()=>{
+        const {method, originalUrl} = req;
+        const {statusCode} = res;
 
+        console.log(`[${startTime}]${method}${originalUrl}${statusCode}`)
+    } )
 
-//Middleware can also be declared in an array for reusability.
-//This example shows an array with a middleware sub-stack that handles GET requests to the /user/:id path
-  function logOriginalUrl (req, res, next) {
+    next();
+
+}
+
+function logOriginalUrl (req, res, next) {
     console.log('Request URL:', req.originalUrl)
     next()
   }
@@ -41,16 +30,37 @@ app.get('/user/:id', (req, res, next) => {
     console.log('Request Type:', req.method)
     next()
   }
-  
+
   const logStuff = [logOriginalUrl, logMethod]
+
+
+app.use(requestLogger);
+app.use(express.json());
+app.use('/api/users', userRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+
+ 
+  
   app.get('/user/:id', logStuff, (req, res, next) => {
     res.send('User Info')
   })
 
-  */
 
-  //Route handlers
-//More than one callback function can handle a route (make sure you specify the next object). For example:
+app.post('/submit-form', (req, res)=>{
+    console.log(req.body);
+    res.send('form has been submitted')
+});
+
+
+app.get('/', (req, res) => {
+    console.log("Root route accessed");
+    res.send("Hello world");
+});
 
 app.get('/example/b', (req, res, next) => {
     console.log('the response will be sent by the next function ...')
@@ -58,3 +68,32 @@ app.get('/example/b', (req, res, next) => {
   }, (req, res) => {
     res.send('Hello from B!')
   })
+
+app.listen(port, () => {
+    console.log(`Example app is listening on port ${port}`)
+});
+
+
+
+app.post('/login', (req, res)=>{
+    console.log(req.body);
+    res.send(req.body);
+})
+
+
+app.post('/tasks', (req, res)=>{
+    const task = {id: new Date, ...req.body}
+    tasks.push(task);
+
+
+    res.status(201).json(task);
+})
+
+app.get('/tasks', (req,res)=>{
+    res.json(tasks)
+})
+
+app.get('/error', (req, res) => {
+    throw new Error('Boom!');
+  });
+  
